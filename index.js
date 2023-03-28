54492d39c31de1e24c304eecce8a1e765ac658a9
“anar <sehar.uzair23@gmail.com>
2023-03-28 14:13:07 -0300

    dependencied added

---
+++ index.js
@@ -0,0 +1,82 @@
+//Include packages needed for this application
+//const filesystem = require('./node_modules/graceful-fs/graceful-fs')
+const inquirer = require('inquirer');
+const fs = require('fs');
+//const shapes = require('./lib/shapes');
+const {Circle, Square, Triangle} = require("./lib/shapes")
+
+// //svg class
+// class Svg{
+//     constructor(){
+//         this.textElement=''
+//         this.shapeElement=''
+//     }
+//     render(){
+//     return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`    }
+//     setTextElement(text,color){
+//         this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+//     }
+//     setShapeElement(shape){
+//         this.shapeElement=shape.render()
+//     }
+// }
+
+// // Create an array of questions for user input//
+
+// const questions = [
+// {
+//     type: `list`,
+//     name: `shape`,
+//     message: `What shape do you like for your logo?`, 
+//     choices:[`Circle`, `Square`, `Triangle`], 
+// },
+// {
+//     type:`input`,
+//     name:`color`,
+//     message:`please enter the color (or a hexadecimal number for color) you want, for your logo. (Required)`,
+//     validate: colorInput=>{
+//         if (colorInput){
+//             return true;
+//         }else{
+//             console.log(`please provide color (or a hexadecimal number for color) of your choice!`);
+//             return false;
+//         }
+//     }
+// },
+// {
+//     type:`input`,
+//     name:`text`,
+//     message:`please provide text for your logo. (Required)`,
+//     validate: textInput=>{
+//         if (textInput){
+//             return true;
+//         }else{
+//             console.log(`please enter the desired text!`);
+//             return false;
+//         }
+//     }
+// },
+// {
+//     type:`input`,
+//     name:`textcolor`,
+//     message:`please enter the color (or a hexadecimal number for color) for text of your logo. (Required)`,
+//     validate: textInput=>{
+//         if (textInput){
+//             return true;
+//         }else{
+//             console.log(`please enter the desired color!(or a hexadecimal number for color)`);
+//             return false;
+//         }
+//     }
+// },
+// ];
+// //function to write file
+// function writeToFile(fileName, data) {
+// 	//console.log("Writing [" + data + "] to file [" + fileName + "]")
+//     filesystem.writeFile(fileName, data, function (err) {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log("you have generated a logo.svg!");
+//     });
+// }
