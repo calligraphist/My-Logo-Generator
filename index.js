f2142670c4ad4db6b8f5dc709aebee8bfbe5dee4
“anar <sehar.uzair23@gmail.com>
2023-03-28 14:20:03 -0300

    questions to prompt added

--- index.js
+++ index.js
@@ -21,55 +21,55 @@ class Svg{
     }
 }
 
-// // Create an array of questions for user input//
+// Create an array of questions for user input//
 
-// const questions = [
-// {
-//     type: `list`,
-//     name: `shape`,
-//     message: `What shape do you like for your logo?`, 
-//     choices:[`Circle`, `Square`, `Triangle`], 
-// },
-// {
-//     type:`input`,
-//     name:`color`,
-//     message:`please enter the color (or a hexadecimal number for color) you want, for your logo. (Required)`,
-//     validate: colorInput=>{
-//         if (colorInput){
-//             return true;
-//         }else{
-//             console.log(`please provide color (or a hexadecimal number for color) of your choice!`);
-//             return false;
-//         }
-//     }
-// },
-// {
-//     type:`input`,
-//     name:`text`,
-//     message:`please provide text for your logo. (Required)`,
-//     validate: textInput=>{
-//         if (textInput){
-//             return true;
-//         }else{
-//             console.log(`please enter the desired text!`);
-//             return false;
-//         }
-//     }
-// },
-// {
-//     type:`input`,
-//     name:`textcolor`,
-//     message:`please enter the color (or a hexadecimal number for color) for text of your logo. (Required)`,
-//     validate: textInput=>{
-//         if (textInput){
-//             return true;
-//         }else{
-//             console.log(`please enter the desired color!(or a hexadecimal number for color)`);
-//             return false;
-//         }
-//     }
-// },
-// ];
+const questions = [
+{
+    type: `list`,
+    name: `shape`,
+    message: `What shape do you like for your logo?`, 
+    choices:[`Circle`, `Square`, `Triangle`], 
+},
+{
+    type:`input`,
+    name:`color`,
+    message:`please enter the color (or a hexadecimal number for color) you want, for your logo. (Required)`,
+    validate: colorInput=>{
+        if (colorInput){
+            return true;
+        }else{
+            console.log(`please provide color (or a hexadecimal number for color) of your choice!`);
+            return false;
+        }
+    }
+},
+{
+    type:`input`,
+    name:`text`,
+    message:`please provide text for your logo. (Required)`,
+    validate: textInput=>{
+        if (textInput){
+            return true;
+        }else{
+            console.log(`please enter the desired text!`);
+            return false;
+        }
+    }
+},
+{
+    type:`input`,
+    name:`textcolor`,
+    message:`please enter the color (or a hexadecimal number for color) for text of your logo. (Required)`,
+    validate: textInput=>{
+        if (textInput){
+            return true;
+        }else{
+            console.log(`please enter the desired color!(or a hexadecimal number for color)`);
+            return false;
+        }
+    }
+},
+];
 // //function to write file
 // function writeToFile(fileName, data) {
 // 	//console.log("Writing [" + data + "] to file [" + fileName + "]")
