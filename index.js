64bb472b774912591e2f4f70155f679ff78d4fa2
“anar <sehar.uzair23@gmail.com>
2023-03-28 14:18:54 -0300

    class svg

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const fs = require('fs');
 //const shapes = require('./lib/shapes');
 const {Circle, Square, Triangle} = require("./lib/shapes")
 
-// //svg class
-// class Svg{
-//     constructor(){
-//         this.textElement=''
-//         this.shapeElement=''
-//     }
-//     render(){
-//     return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`    }
-//     setTextElement(text,color){
-//         this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-//     }
-//     setShapeElement(shape){
-//         this.shapeElement=shape.render()
-//     }
-// }
+//svg class
+class Svg{
+    constructor(){
+        this.textElement=''
+        this.shapeElement=''
+    }
+    render(){
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`    }
+    setTextElement(text,color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement=shape.render()
+    }
+}
 
 // // Create an array of questions for user input//
 
